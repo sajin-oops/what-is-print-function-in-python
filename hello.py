ce1c072f3b7a3af29a4e48c48db983d7a8c13f3c
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-06 16:41:25 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("hello world") #string - set of characters  #-comment
+print("Sajin is a good boy")
+
+
+#print means what?
+# print is a function
+# functions has open and closing paranthesis
+# its a inbuilt function and we call that function to do some works
